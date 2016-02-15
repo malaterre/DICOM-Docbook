9c19f0af66cf799d4c1b64078cca76f32908991b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-02-15 12:53:51 +0100

    Add script

---
+++ support/build/grab.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+set -x
+
+wget -c http://dicom.nema.org/medical/dicom/current/DocBookDICOM2015c_sourceandrenderingpipeline_20150705073023.tar.bz2
+
+mkdir -p tmp
+tar -C tmp -x -f DocBookDICOM2015c_sourceandrenderingpipeline_20150705073023.tar.bz2
+
+rm -rf tmp/docbook-xsl-ns-1.78.1
+rm -rf tmp/lib
+rm -rf tmp/pMML2SVG-0.8.5
+rm -rf tmp/XEP_trial
+rm -rf tmp/catalogs
+rm -rf tmp/wordexport
+rm -rf tmp/stylesheets
+rm -rf tmp/svg2emf
+
+# do not mess with the current one:
+rm -rf tmp/source
+rm -rf tmp/support
+
+mv tmp/* .

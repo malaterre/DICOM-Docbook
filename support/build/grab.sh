9af04a2be502fa2aa515043ddc12a8b85c0013b8
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-02-15 15:28:31 +0100

    update script

--- support/build/grab.sh
+++ support/build/grab.sh
@@ -9,7 +9,11 @@ tar -C tmp -x -f DocBookDICOM2015c_sourceandrenderingpipeline_20150705073023.tar
 
 rm -rf tmp/docbook-xsl-ns-1.78.1
 rm -rf tmp/lib
-rm -rf tmp/pMML2SVG-0.8.5
+#rm -rf tmp/pMML2SVG-0.8.5/
+rm -rf tmp/pMML2SVG-0.8.5/examples
+rm -rf tmp/pMML2SVG-0.8.5/doc
+rm -rf tmp/pMML2SVG-0.8.5/ttfreader
+rm     tmp/pMML2SVG-0.8.5/*
 rm -rf tmp/XEP_trial
 rm -rf tmp/catalogs
 rm -rf tmp/wordexport
@@ -21,3 +25,8 @@ rm -rf tmp/source
 rm -rf tmp/support
 
 mv tmp/* .
+
+#TODO:
+#CUR=$PWD
+#sed -i -e "s:/Users/dclunie/Documents/Work/DICOM_Publish_XML/DocBookDICOM2013/:$CUR:" ./olinkdb_pdf.xml
+#sed -i -e 's:/opt/local/share/java/saxon9he.jar:/usr/share/java/Saxon-HE.jar:' *.sh
